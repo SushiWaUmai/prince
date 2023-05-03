832710006fe4322edffa5feb3035e30fe4e608f4
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-04 00:24:30 +0200

    feat: :sparkles: implement a very basic tts

---
+++ commands/tts.go
@@ -0,0 +1,99 @@
+package commands
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+
+	"os"
+	"strings"
+
+	"github.com/hegedustibor/htgo-tts"
+	"github.com/hegedustibor/htgo-tts/voices"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/types/events"
+	"google.golang.org/protobuf/proto"
+)
+
+func init() {
+	createCommand("tts", func(client *whatsmeow.Client, messageEvent *events.Message, ctx *waProto.ContextInfo, args []string) {
+		if len(args) <= 0 {
+			client.SendMessage(context.Background(), messageEvent.Info.Chat, &waProto.Message{
+				Conversation: proto.String("Please specify a text to speak"),
+			})
+			return
+		}
+
+		text := strings.Join(args, " ")
+
+		speech := htgotts.Speech{
+			Folder:   ".",
+			Language: voices.English,
+		}
+
+		speech.CreateSpeechFile(text, "speach")
+		defer os.Remove("speach.mp3")
+
+		// get the bytes
+		audioBytes, err := os.ReadFile("speach.mp3")
+		audioBytes, err = mp3ToOgg(audioBytes)
+
+		uploadResp, err := client.Upload(context.Background(), audioBytes, whatsmeow.MediaAudio)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		audioMsg := &waProto.AudioMessage{
+			Mimetype:      proto.String("audio/ogg; codecs=opus"),
+			Url:           &uploadResp.URL,
+			DirectPath:    &uploadResp.DirectPath,
+			MediaKey:      uploadResp.MediaKey,
+			FileEncSha256: uploadResp.FileEncSHA256,
+			FileSha256:    uploadResp.FileSHA256,
+			FileLength:    &uploadResp.FileLength,
+		}
+
+		_, err = client.SendMessage(context.Background(), messageEvent.Info.Chat, &waProto.Message{
+			AudioMessage: audioMsg,
+		})
+
+		if err != nil {
+			log.Println(err)
+		}
+	})
+}
+
+func mp3ToOgg(audioData []byte) ([]byte, error) {
+	// ffmpeg -i $inFileName -acodec libmp3lame -y $outFileName
+	tmpFile, err := os.CreateTemp("", "audio*.mp3")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tmpFile.Name())
+
+	_, err = io.Copy(tmpFile, bytes.NewReader(audioData))
+	if err != nil {
+		return nil, err
+	}
+
+	tmpFileOut, err := os.CreateTemp("", "audio*.ogg")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tmpFileOut.Name())
+
+	err = ffmpeg.Input(tmpFile.Name()).Output(tmpFileOut.Name(), ffmpeg.KwArgs{
+		"acodec": "libmp3lame",
+		"c:a": "libopus",
+	}).OverWriteOutput().Run()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return os.ReadFile(tmpFileOut.Name())
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,13 @@ go 1.20
 
 require (
 	github.com/chai2010/webp v1.1.1
+	github.com/hegedustibor/htgo-tts v0.0.0-20230402053941-cd8d1a158135
+	github.com/ipinfo/go/v2 v2.9.2
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/mdp/qrterminal v1.0.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/sashabaranov/go-openai v1.7.0
-	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c
 	github.com/u2takey/ffmpeg-go v0.4.1
 	go.mau.fi/whatsmeow v0.0.0-20230410091758-46e30e265256
 	google.golang.org/protobuf v1.28.1
@@ -21,20 +22,19 @@ require (
 	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/ipinfo/go/v2 v2.9.2 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
+	github.com/hajimehoshi/oto/v2 v2.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	go.mau.fi/libsignal v0.1.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
