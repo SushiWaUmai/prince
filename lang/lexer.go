cf16698c46ce68c04e775bb34b165db52affcfdb
Eugene Matsumura <eugene.bb1@gmail.com>
2023-08-18 07:49:32 +0200

    feat: add simple lexer

--- lang/lexer.go
+++ lang/lexer.go
@@ -1,5 +1,130 @@
 package lang
 
-func lex(content string) {
+import (
+	"unicode"
+)
+
+type TokenType int64
+
+const (
+	IDENTIFIER TokenType = iota
+	SEPARATOR
+	EOF
+)
+
+type Token struct {
+	Type  TokenType
+	Lexme string
 }
 
+func isAlphaNumeric(c byte) bool {
+	return unicode.IsLetter(rune(c)) || unicode.IsDigit(rune(c))
+}
+
+func isWhiteSpace(c byte) bool {
+	return unicode.IsSpace(rune(c))
+}
+
+func isQuotation(c byte) bool {
+	return c == '"'
+}
+
+func StringTilChar(content string, i int, startCond func(c byte) bool, endCond func(c byte) bool) (string, int) {
+	var c byte
+
+	c = content[i]
+	length := len(content)
+	lexme := ""
+
+	if startCond(c) {
+		c = content[i]
+		for {
+			lexme += string(c)
+			i++
+			if i >= length {
+				break
+			}
+
+			c = content[i]
+			if endCond(c) {
+				lexme += string(c)
+				break
+			}
+		}
+	}
+
+	return lexme, i
+}
+
+func LexIdentifier(tokens []Token, content string, i int) ([]Token, int, bool) {
+	lexme, i := StringTilChar(content, i, isAlphaNumeric, isWhiteSpace)
+	result := false
+	if lexme != "" {
+		if i < len(content) {
+			lexme = lexme[:len(lexme)-1]
+		}
+
+		tokens = append(tokens, Token{
+			Type:  IDENTIFIER,
+			Lexme: lexme,
+		})
+		result = true
+	}
+
+	return tokens, i, result
+}
+
+func LexString(tokens []Token, content string, i int) ([]Token, int, bool) {
+	lexme, i := StringTilChar(content, i, isQuotation, isQuotation)
+	result := false
+	if lexme != "" {
+		lexme = lexme[1 : len(lexme)-1]
+		tokens = append(tokens, Token{
+			Type:  IDENTIFIER,
+			Lexme: lexme,
+		})
+		result = true
+	}
+
+	return tokens, i, result
+}
+
+func LexPipe(tokens []Token, content string, i int) ([]Token, int, bool) {
+	c := content[i]
+	result := false
+	if c == '|' || c == '!' {
+		tokens = append(tokens, Token{
+			Type:  SEPARATOR,
+			Lexme: string(c),
+		})
+		result = true
+	}
+
+	return tokens, i, result
+}
+
+func Lex(content string) []Token {
+	var tokens []Token
+	var result bool
+
+	length := len(content)
+	for i := 0; i < length; i++ {
+		tokens, i, result = LexPipe(tokens, content, i)
+		if result {
+			continue
+		}
+
+		tokens, i, result = LexString(tokens, content, i)
+		if result {
+			continue
+		}
+
+		tokens, i, result = LexIdentifier(tokens, content, i)
+		if result {
+			continue
+		}
+	}
+	tokens = append(tokens, Token{EOF, ""})
+
+	return tokens
+}
--- lang/lexer_test.go
+++ lang/lexer_test.go
@@ -3,12 +3,112 @@ package lang
 import (
 	"testing"
 
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLexer(t *testing.T) {
-	t.Run("Process simple command", func(t *testing.T) {
-		// assert := assert.New(t)
-		// sample := "ping hello world"
+	t.Run("Test LexIdentifier", func(t *testing.T) {
+		assert := assert.New(t)
+		sample := "hello"
+
+		tokens := []Token{}
+		i := 0
+		tokens, i, result := LexIdentifier(tokens, sample, i)
+		assert.True(result)
+		assert.Equal(5, i)
+		assert.Equal([]Token{
+			{
+				Type:  IDENTIFIER,
+				Lexme: "hello",
+			},
+		}, tokens)
+	})
+
+	t.Run("Test LexIdentifier with simple command", func(t *testing.T) {
+		assert := assert.New(t)
+		sample := "ping hello world"
+
+		tokens := []Token{}
+		i := 0
+		tokens, i, result := LexIdentifier(tokens, sample, i)
+		assert.True(result)
+		assert.Equal(4, i)
+		assert.Equal([]Token{
+			{
+				Type:  IDENTIFIER,
+				Lexme: "ping",
+			},
+		}, tokens)
+	})
+
+	t.Run("Test LexPipe", func(t *testing.T) {
+		assert := assert.New(t)
+		sample := "|"
+
+		tokens := []Token{}
+		i := 0
+		tokens, i, result := LexPipe(tokens, sample, i)
+		assert.True(result)
+		assert.Equal(0, i)
+		assert.Equal([]Token{
+			{
+				Type:  SEPARATOR,
+				Lexme: "|",
+			},
+		}, tokens)
+	})
+
+	t.Run("Test LexString", func(t *testing.T) {
+		assert := assert.New(t)
+		sample := "\"hello world\""
+
+		tokens := []Token{}
+		i := 0
+		tokens, i, result := LexString(tokens, sample, i)
+		assert.True(result)
+		assert.Equal(12, i)
+		assert.Equal([]Token{
+			{
+				Type:  IDENTIFIER,
+				Lexme: "hello world",
+			},
+		}, tokens)
+	})
+
+	t.Run("Test Lex with simple command", func(t *testing.T) {
+		assert := assert.New(t)
+		sample := "!ping hello | ping \"hello world\""
+
+		tokens := Lex(sample)
+		assert.Equal([]Token{
+			{
+				Type:  SEPARATOR,
+				Lexme: "!",
+			},
+			{
+				Type:  IDENTIFIER,
+				Lexme: "ping",
+			},
+			{
+				Type:  IDENTIFIER,
+				Lexme: "hello",
+			},
+			{
+				Type:  SEPARATOR,
+				Lexme: "|",
+			},
+			{
+				Type:  IDENTIFIER,
+				Lexme: "ping",
+			},
+			{
+				Type:  IDENTIFIER,
+				Lexme: "hello world",
+			},
+			{
+				Type:  EOF,
+				Lexme: "",
+			},
+		}, tokens)
 	})
 }
