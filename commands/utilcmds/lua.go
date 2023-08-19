a18478a913c36b1590e38aa4d39038e483407665
Eugene Matsumura <eugene.bb1@gmail.com>
2023-08-19 23:35:50 +0200

    feat: make lua command runnable with sendMessage

--- commands/utilcmds/lua.go
+++ commands/utilcmds/lua.go
@@ -1,22 +1,43 @@
 package utilcmds
 
 import (
+	"context"
+	"errors"
+	"strings"
+
 	"github.com/SushiWaUmai/prince/utils"
 	"github.com/yuin/gopher-lua"
 	"go.mau.fi/whatsmeow"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/types"
+	"layeh.com/gopher-luar"
 )
 
 func LuaCommand(client *whatsmeow.Client, chat types.JID, user string, ctx *waProto.ContextInfo, pipe *waProto.Message, args []string) (*waProto.Message, error) {
+	var script string
+	if len(args) > 0 {
+		script = strings.Join(args, " ")
+	} else {
+		script, _ = utils.GetTextContext(pipe)
+	}
+	script = strings.TrimSpace(script)
+
+	if script == "" {
+		return utils.CreateTextMessage("Please input a lua script"), errors.New("No script specified")
+	}
+
+	sendMessage := func(text string) {
+		client.SendMessage(context.Background(), chat, utils.CreateTextMessage(text))
+	}
+
 	L := lua.NewState()
 	defer L.Close()
 
-	err := L.DoString(`print("hello")`); 
-
+	L.SetGlobal("sendMessage", luar.New(L, sendMessage))
+	err := L.DoString(script)
 
 	if err != nil {
-		return nil, err
+		return utils.CreateTextMessage(err.Error()), err
 	}
 
 	return nil, nil
--- go.mod
+++ go.mod
@@ -17,10 +17,12 @@ require (
 	github.com/stretchr/testify v1.8.4
 	github.com/u2takey/ffmpeg-go v0.4.1
 	github.com/wader/goutubedl v0.0.0-20230503164948-cef23e8d43d4
+	github.com/yuin/gopher-lua v1.1.0
 	go.mau.fi/whatsmeow v0.0.0-20230410091758-46e30e265256
 	google.golang.org/protobuf v1.28.1
 	gorm.io/driver/sqlite v1.5.0
 	gorm.io/gorm v1.25.0
+	layeh.com/gopher-luar v1.0.11
 	mvdan.cc/xurls/v2 v2.5.0
 )
 
@@ -40,11 +42,9 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
-	github.com/yuin/gopher-lua v1.1.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.mau.fi/libsignal v0.1.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
