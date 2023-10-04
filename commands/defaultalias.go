26da9fbd2d3946d17b9499991f60a344dd90f53b
Eugene Matsumura <eugene.bb1@gmail.com>
2023-10-04 22:12:16 +0200

    feat: move zundamon to voicevox command

--- commands/defaultalias.go
+++ commands/defaultalias.go
@@ -5,4 +5,5 @@ import "github.com/SushiWaUmai/prince/db"
 func init() {
 	db.CreateAlias("autodownload", "onmessage download")
 	db.CreateAlias("autopilot", "onmessage chat")
+	db.CreateAlias("zundamon", "voicevox 1")
 }
--- commands/mediacmds/voicevox.go
+++ commands/mediacmds/voicevox.go
@@ -2,6 +2,7 @@ package mediacmds
 
 import (
 	"errors"
+	"strconv"
 	"strings"
 
 	"github.com/SushiWaUmai/prince/utils"
@@ -11,13 +12,16 @@ import (
 )
 
 var voicevox = utils.CreateVoiceVoxClient()
-var zundamonIdx = 1
 
-// TODO: Change this to voicevox command and add zundamon alias
-func ZundamonCommand(client *whatsmeow.Client, chat types.JID, user string, ctx *waProto.ContextInfo, pipe *waProto.Message, args []string) (*waProto.Message, error) {
+func VoicevoxCommand(client *whatsmeow.Client, chat types.JID, user string, ctx *waProto.ContextInfo, pipe *waProto.Message, args []string) (*waProto.Message, error) {
+	if len(args) <= 0 {
+		return nil, errors.New("Please enter a valid speaker index")			
+	}
+
 	var text string
-	if len(args) > 0 {
-		text = strings.Join(args, " ")
+	speakerIndex, err := strconv.Atoi(args[0])
+	if len(args) > 1 {
+		text = strings.Join(args[1:], " ")
 	} else {
 		text, _ = utils.GetTextContext(pipe)
 	}
@@ -27,13 +31,13 @@ func ZundamonCommand(client *whatsmeow.Client, chat types.JID, user string, ctx 
 		return utils.CreateTextMessage("Please specify a text to speak"), errors.New("No Text specified")
 	}
 
-	query, err := voicevox.CreateQuery(zundamonIdx, text)
+	query, err := voicevox.CreateQuery(speakerIndex, text)
 
 	if err != nil {
 		return nil, err
 	}
 
-	buffer, err := voicevox.CreateVoice(1, true, query)
+	buffer, err := voicevox.CreateVoice(speakerIndex, true, query)
 	if err != nil {
 		return nil, err
 	}
@@ -44,5 +48,5 @@ func ZundamonCommand(client *whatsmeow.Client, chat types.JID, user string, ctx 
 }
 
 func init() {
-	utils.CreateCommand("zundamon", "USER", "", ZundamonCommand)
+	utils.CreateCommand("voicevox", "USER", "Converts text to speech using specified speaker index and message.", VoicevoxCommand)
 }
