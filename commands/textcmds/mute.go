eb93133cb3bca7c977ee37f359425b2d96766b2c
Eugene Matsumura <eugene.bb1@gmail.com>
2023-11-05 18:02:48 +0100

    feat: add mute command

---
+++ commands/textcmds/mute.go
@@ -0,0 +1,16 @@
+package textcmds
+
+import (
+	"github.com/SushiWaUmai/prince/utils"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/types"
+)
+
+func MuteCommand(client *whatsmeow.Client, chat types.JID, user string, ctx *waProto.ContextInfo, pipe *waProto.Message, args []string) (*waProto.Message, error) {
+	return nil, nil
+}
+
+func init() {
+	utils.CreateCommand("mute", "USER", "Mutes pipe", MuteCommand)
+}
